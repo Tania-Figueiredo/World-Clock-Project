74838027331a868e71ceaad73d61e4e043227c3b
TaniaFigueiredo <taniacnfigueiredo@gmail.com>
2025-05-29 21:28:15 +0100

    added a button, and current location time and date.

--- src/index.js
+++ src/index.js
@@ -1,71 +1,70 @@
 function updateTime(cityId, timeZone) {
   let dateElement = document.querySelector(`#${cityId}-date`);
   let timeElement = document.querySelector(`#${cityId}-time`);
-  if (!dateElement || !timeElement) return;
 
-  let cityTime = moment().tz(timeZone);
-  dateElement.innerHTML = cityTime.format("MMMM Do YYYY");
-  timeElement.innerHTML = cityTime.format("HH:mm:ss");
+  if (dateElement && timeElement) {
+    let time = moment().tz(timeZone);
+    dateElement.innerHTML = time.format("MMMM Do YYYY");
+    timeElement.innerHTML = time.format("HH:mm:ss");
+  }
 }
 
-function updateCityTimes() {
+function updateAllTimes() {
   updateTime("lisbon", "Europe/Lisbon");
   updateTime("madrid", "Europe/Madrid");
   updateTime("paris", "Europe/Paris");
   updateTime("rome", "Europe/Rome");
 }
+updateAllTimes();
+setInterval(updateAllTimes, 1000);
 
-updateCityTimes();
-let citiesInterval = setInterval(updateCityTimes, 1000);
+function showSingleCity(timeZone, cityName) {
+  const grid = document.querySelector("#cities-grid");
+  const single = document.querySelector("#single-city");
+  const back = document.querySelector("#back-button-wrapper");
 
-let citySelect = document.querySelector("#city-select");
+  grid.style.display = "none";
+  back.classList.remove("hidden");
 
-citySelect.addEventListener("change", function () {
-  let selectedTimeZone = this.value;
-
-  if (!selectedTimeZone) {
-    let dynamicCity = document.querySelector(".dynamic-city");
-    if (dynamicCity) {
-      dynamicCity.remove();
-    }
-
-    document.querySelector(".cities-grid").style.display = "grid";
-
-    updateCityTimes();
-    citiesInterval = setInterval(updateCityTimes, 1000);
-    return;
-  }
-
-  clearInterval(citiesInterval);
-
-  document.querySelector(".cities-grid").style.display = "none";
-
-  let previousCity = document.querySelector(".dynamic-city");
-  if (previousCity) {
-    previousCity.remove();
-  }
-
-  let cityElement = document.createElement("div");
-  cityElement.classList.add("city", "dynamic-city");
-
-  let cityName = selectedTimeZone.split("/")[1].replace("_", " ");
-  cityElement.innerHTML = `
-    <h2>${cityName}</h2>
-    <div class="date" id="dynamic-date"></div>
-    <div class="time" id="dynamic-time"></div>
+  let cityTime = moment().tz(timeZone);
+  single.innerHTML = `
+    <div class="city">
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      <div class="time">${cityTime.format("HH:mm:ss")}</div>
+    </div>
   `;
 
-  document.querySelector(".container").appendChild(cityElement);
-
-  function updateSelectedCity() {
-    let cityTime = moment().tz(selectedTimeZone);
-    document.querySelector("#dynamic-date").innerHTML =
-      cityTime.format("MMMM Do YYYY");
-    document.querySelector("#dynamic-time").innerHTML =
-      cityTime.format("HH:mm:ss");
+  function updateSingle() {
+    cityTime = moment().tz(timeZone);
+    single.querySelector(".date").innerHTML = cityTime.format("MMMM Do YYYY");
+    single.querySelector(".time").innerHTML = cityTime.format("HH:mm:ss");
   }
 
-  updateSelectedCity();
+  setInterval(updateSingle, 1000);
+}
 
-  citiesInterval = setInterval(updateSelectedCity, 1000);
+document
+  .querySelector("#city-select")
+  .addEventListener("change", function (event) {
+    const timeZone = event.target.value;
+
+    if (!timeZone) return;
+
+    if (timeZone === "current") {
+      const currentZone = moment.tz.guess();
+      const cityName =
+        currentZone.replace("_", " ").split("/")[1] || "Current Location";
+      showSingleCity(currentZone, cityName);
+    } else {
+      const cityName = event.target.options[event.target.selectedIndex].text;
+      showSingleCity(timeZone, cityName);
+    }
+  });
+
+document.querySelector("#back-button").addEventListener("click", function () {
+  document.querySelector("#single-city").innerHTML = "";
+  document.querySelector("#cities-grid").style.display = "grid";
+  document.querySelector("#back-button-wrapper").classList.add("hidden");
+  document.querySelector("#city-select").value = "";
 });
